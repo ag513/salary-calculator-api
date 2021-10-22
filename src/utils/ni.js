aaa1cf8ebc506ab8903a8f944c232fd609d3e40b
Abhinav Gunishety <a.gunishetty@gmail.com>
2021-10-22 19:57:41 +0100

    initialize all var at first and return

--- src/utils/ni.js
+++ src/utils/ni.js
@@ -1,35 +1,30 @@
 const ni = (salary) => {
     const NIRate = 0.12;
     const NIConcPay = 9568
+    let yearly, monthly, weekly, daily, hourly
+
     if (salary > NIConcPay) {
         const NITaxableIncome = salary - NIConcPay
         const NIForTaxableIncome = NITaxableIncome * NIRate
-        let yearly = parseFloat(NIForTaxableIncome.toFixed(2))
-        let monthly = parseFloat(((NIForTaxableIncome) / 12).toFixed(2))
-        let weekly = parseFloat(((NIForTaxableIncome) / 52).toFixed(2))
-        let daily = parseFloat(((NIForTaxableIncome) / 355).toFixed(2))
-        let hourly = parseFloat(((NIForTaxableIncome) / 1950).toFixed(2))
-        return {
-            yearly,
-            monthly,
-            weekly,
-            daily,
-            hourly
-        }
+        yearly = parseFloat(NIForTaxableIncome.toFixed(2))
+        monthly = parseFloat(((NIForTaxableIncome) / 12).toFixed(2))
+        weekly = parseFloat(((NIForTaxableIncome) / 52).toFixed(2))
+        daily = parseFloat(((NIForTaxableIncome) / 355).toFixed(2))
+        hourly = parseFloat(((NIForTaxableIncome) / 1950).toFixed(2))
     } else {
         let NIForTaxableIncome = salary * NIRate
-        let yearly = parseFloat(NIForTaxableIncome)
-        let monthly = parseFloat(((NIForTaxableIncome) / 12).toFixed(2))
-        let weekly = parseFloat(((NIForTaxableIncome) / 52).toFixed(2))
-        let daily = parseFloat(((NIForTaxableIncome) / 355).toFixed(2))
-        let hourly = parseFloat(((NIForTaxableIncome) / 1950).toFixed(2))
-        return {
-            yearly,
-            monthly,
-            weekly,
-            daily,
-            hourly
-        }
+        yearly = parseFloat(NIForTaxableIncome)
+        monthly = parseFloat(((NIForTaxableIncome) / 12).toFixed(2))
+        weekly = parseFloat(((NIForTaxableIncome) / 52).toFixed(2))
+        daily = parseFloat(((NIForTaxableIncome) / 355).toFixed(2))
+        hourly = parseFloat(((NIForTaxableIncome) / 1950).toFixed(2))
+    }
+    return {
+        yearly,
+        monthly,
+        weekly,
+        daily,
+        hourly
     }
 }
 
